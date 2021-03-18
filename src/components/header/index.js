2ae7ce31cacdab53836228a807f312178d977e7c
gearoffortune <hopelessuntildeath@gmail.com>
2021-03-18 11:20:12 +0300

    added types and extracted two components

---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import {html} from 'lit-html'
+import './styles.css'
+/**
+ * 
+ * @param {string} title 
+ * @param {string} subtitle 
+ */
+export const header = (title, subtitle) => html`<div class="header">
+<h1 class="header__mainheader">${title}</h1>
+<h2 class="header__secondaryheader">${subtitle}</h2>
+</div>`
---
+++ src/components/person/index.js
@@ -0,0 +1,19 @@
+import {html} from 'lit-html'
+import './styles.css'
+/**
+ * 
+ * @param {string} parentBlockName 
+ * @param {import(../../../stories.d.ts).User} userObj
+ * @param {boolean} isEmojiNeeded 
+ * @param {string} emoji 
+ * @returns 
+ */
+export const person = (parentBlockName, {name, avatar, valueText, id}, isEmojiNeeded, emoji) => html`
+<div class="${parentBlockName}__person person">
+  <div class="${parentBlockName}__person__imgwrapper person__imgwrapper">
+    ${isEmojiNeeded ? html`<div class="${parentBlockName}__person__imgwrapper__emoji person__imgwrapper__emoji">${emoji}</div>` : html``}
+    <img src="/images/1x/${avatar}" alt="" class="${parentBlockName}__person__imgwrapper__img person__imgwrapper__img">
+  </div>
+  <p class="${parentBlockName}__person__name person__name">${name}</p>
+  <p class="${parentBlockName}__person__number person__number">${valueText}</p>
+</div>`
