cc1a8680d1ddfcb6ac106c5a7d59234b9b9c7169
gearoffortune <hopelessuntildeath@gmail.com>
2021-03-28 21:24:20 +0300

    added retina and tried to rebuild vote

--- src/components/person/index.js
+++ src/components/person/index.js
@@ -13,7 +13,11 @@ export const person = (parentBlockName, {name, avatar, valueText, id}, isEmojiNe
 <div class="${parentBlockName}__person person ${isActive ? 'person--active' :''}">
   <div class="${parentBlockName}__person__imgwrapper person__imgwrapper">
     ${isEmojiNeeded ? html`<div class="${parentBlockName}__person__imgwrapper__emoji person__imgwrapper__emoji">${emoji}</div>` : html``}
-    <img src="/images/1x/${avatar}" alt="" class="${parentBlockName}__person__imgwrapper__img person__imgwrapper__img">
+    <img 
+    src="/images/1x/${avatar}" 
+    srcset="/images/2x/${avatar} 2x, /images/3x/${avatar} 3x, /images/4x/${avatar} 4x"
+    alt="" 
+    class="${parentBlockName}__person__imgwrapper__img person__imgwrapper__img">
   </div>
   <p class="${parentBlockName}__person__name person__name">${name}</p>
   <p class="${parentBlockName}__person__number person__number">${valueText}</p>
